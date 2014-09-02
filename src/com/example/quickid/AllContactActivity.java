2bb9f1d664470b8f091f8102d27f230dcb3491fd
NashLegend <panzhihuipzh@gmail.com>
2014-09-02 11:06:18 +0800

    use system vibration and keytone setting and add sidebar to allcontactsaclistview

--- src/com/example/quickid/AllContactActivity.java
+++ src/com/example/quickid/AllContactActivity.java
@@ -2,13 +2,15 @@
 package com.example.quickid;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
-import com.example.legendutils.Tools.ToastUtil;
 import com.example.quickid.adapter.AllContactsAdapter;
 import com.example.quickid.model.Contact;
 import com.example.quickid.util.Consts;
 import com.example.quickid.util.ContactHelper;
 import com.example.quickid.util.PinnedHeaderListView;
+import com.example.quickid.view.SideBar;
 
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -17,16 +19,15 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.SparseArray;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
 
 public class AllContactActivity extends Activity {
     private ContactUpdateReceiver receiver;
     private final SparseArray<ArrayList<Contact>> contactMaps = new SparseArray<ArrayList<Contact>>();
     private final SparseArray<String> charMaps = new SparseArray<String>();
+    private final HashMap<String, Integer> keyMaps = new HashMap<String, Integer>();
     PinnedHeaderListView listView;
     AllContactsAdapter adapter;
+    SideBar sideBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +40,33 @@ public class AllContactActivity extends Activity {
         adapter.setData(contactMaps, charMaps);
         listView.setAdapter(adapter);
 
+        int pos = 0;
+        for (int i = 0; i < charMaps.size(); i++) {
+            int idx = charMaps.keyAt(i);
+            keyMaps.put(charMaps.get(idx), pos);
+            pos += contactMaps.get(idx).size() + 1;
+        }
+
+        sideBar = (SideBar) findViewById(R.id.sidebar_all_contacts);
+
+        sideBar.setOnTouchingLetterChangedListener(new OnTouchingLetterChangedListener());
+
         IntentFilter filter = new IntentFilter();
         filter.addAction(Consts.Action_All_Contacts_Changed);
         receiver = new ContactUpdateReceiver();
         registerReceiver(receiver, filter);
+    }
+
+    class OnTouchingLetterChangedListener implements SideBar.OnTouchingLetterChangedListener {
+
+        @Override
+        public void onTouchingLetterChanged(final String s) {
+            if (keyMaps.get(s) != null) {
+                int idx = keyMaps.get(s);
+                listView.setSelection(idx);
+            }
+        }
+
     }
 
     private void updateData() {
